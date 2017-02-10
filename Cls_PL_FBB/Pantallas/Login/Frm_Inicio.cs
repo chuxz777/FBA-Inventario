d6dc0e132a7138b053ad3e9e735d0bc3c7fbbc60
Jesus <jesus.rojas.umana@gmail.com>
2017-02-10 00:11:33 -0600

    Acomodando Menu y Pantalla de Inicio

--- Cls_PL_FBB/Pantallas/Login/Frm_Inicio.cs
+++ Cls_PL_FBB/Pantallas/Login/Frm_Inicio.cs
@@ -16,5 +16,10 @@ namespace FBA_Inventario.Pantallas.Login
         {
             InitializeComponent();
         }
+
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+  
+        }
     }
 }
--- Cls_PL_FBB/Program.cs
+++ Cls_PL_FBB/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Cls_PL;
+using FBA_Inventario.Pantallas.Login;
 
 namespace Cls_PL
 {
@@ -16,7 +18,7 @@ namespace Cls_PL
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new  Frm_Inicio ());
         }
     }
 }
