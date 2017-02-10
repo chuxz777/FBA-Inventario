18032d7e771e3ed9558ccb59c8ba7ebdfeb88599
Jesus <jesus.rojas.umana@gmail.com>
2017-02-09 23:48:47 -0600

    actualizar cascaron

--- FBA-Inventario/Program.cs
+++ FBA-Inventario/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FBA_Inventario.Pantallas.Login;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -16,7 +17,7 @@ namespace FBA_Inventario
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Frm_Inicio());
         }
     }
 }
